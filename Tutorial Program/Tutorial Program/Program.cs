ffffe5574bdc567074e353b2655783f9daf54349
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2020-11-12 21:30:17 -0500

    Add files via upload

---
+++ Tutorial Program/Tutorial Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tutorial_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("  /");
+            Console.WriteLine(" /");
+            Console.WriteLine("  /");
+
+            string characterName;
+
+            Console.WriteLine("Type A Name");
+
+
+            Console.ReadLine();
+        }
+    }
+}
